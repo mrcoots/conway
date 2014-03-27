2119eeac98ff63cf76b81fa3c8fc231d3b2108d1
Martijn <mvancooten@davinci.nl>
2014-03-27 09:51:58 +0100

    create grid, create for lus

--- conway/ConsoleApplication1/Program.cs
+++ conway/ConsoleApplication1/Program.cs
@@ -10,6 +10,24 @@ namespace Conway
     {
         static void Main(string[] args)
         {
+            var grid = new bool[4, 4]
+            {
+                {true, false, false, true },
+                {false, false, true, false },
+                {true, false, true, true },
+                {false, true, false, true }
+            };
+            for (int k = 0; k < 10; k++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
