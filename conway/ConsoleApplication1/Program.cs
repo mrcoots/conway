194146507bd5bc42b6504e827a33baff206c3757
Martijn <mvancooten@davinci.nl>
2014-03-27 11:05:20 +0100

    Code to control if neighbor is alife or dead

--- conway/ConsoleApplication1/Program.cs
+++ conway/ConsoleApplication1/Program.cs
@@ -22,9 +22,9 @@ namespace Conway
                 for (int i = 0; i <4; i++)
                 {
                     if (grid[k, i] == true)
-                        {
-                    Console.Write("X");
-                        }
+                    {
+                        Console.Write("X");
+                    }
                     else if (grid[k, i] == false)
                     {
                         Console.Write(" ");
@@ -35,6 +35,57 @@ namespace Conway
 
             Console.ReadLine();
 
+        }
+
+        private static int CountNeighbors(bool[,] grid, int x, int y)
+        {
+            int livingNeighborCount = 0;
+            bool atLeftEdge = (x == 0);
+            bool atTopEdge = (y == 0);
+            bool atRightEdge = (x == grid.GetLength(0) - 1);
+            bool atBottomEdge = (y == grid.GetLength(1) - 1);
+
+            if (!atLeftEdge && !atTopEdge && grid[x - 1, y - 1])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atTopEdge && grid[x, y - 1])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atRightEdge && !atTopEdge && grid[x + 1, y - 1])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atLeftEdge && grid[x - 1, y])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atRightEdge && grid[x + 1, y])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atLeftEdge && !atBottomEdge && grid[x - 1, y + 1])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atBottomEdge && grid[x, y + 1])
+            {
+                livingNeighborCount++;
+            }
+
+            if (!atRightEdge && !atBottomEdge && grid[x + 1, y + 1])
+            {
+                livingNeighborCount++;
+            }
+
+            return livingNeighborCount;
         }
     }
 }
